13d38d9377bae03382b80c1b7773d5573654a944
shawn a meighn <smeighn@gmail.com>
2019-01-17 21:07:16 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
 # Define the variable board below.
 class Board
-  attr_accessor :board, :winner_of_game #attr_accessor turn instance variable into instance method ( like @ no longer required ) in the class definition.
-
-  def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] #simply returning instance variable @board
-    @winner_of_game = nil
-end
+  attr_reader :rows
+  attr_reader :columns
+  attr_reader :board
+  def initialize (rows, columns)
+    @rows = rows
+    @columns = columns
+    @board = Array.new(rows) {Array.new(columns)}
+    create
+  end
