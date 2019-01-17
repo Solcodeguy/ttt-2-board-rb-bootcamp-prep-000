2e71a9a5c4082fd1246dac6184f6b014eb08fbaa
shawn a meighn <smeighn@gmail.com>
2019-01-17 21:13:15 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,1 @@
 # Define the variable board below.
-class Board
-  attr_reader :rows
-  attr_reader :columns
-  attr_reader :board
-  def initialize (rows, columns)
-    @rows = rows
-    @columns = columns
-    @board = Array.new(rows) {Array.new(columns)}
-    create
-  end
